fca681e522a9331b9323a0f12a98708f0e02b390
Bajishaik85 <55596188+Bajishaik85@users.noreply.github.com>
2023-01-05 15:32:33 +0530

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -5,7 +5,7 @@ const Login = (props) => {
     const navigate = useNavigate();
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("http://localhost:5000/api/v1/auth/login", {
+        const response = await fetch("https://inotebook-backend-82ye.onrender.com/api/v1/auth/login", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
