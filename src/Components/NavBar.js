a9a4c409ee9c8e820937c1e88205acd38bce67c6
Bajishaik85 <55596188+Bajishaik85@users.noreply.github.com>
2023-01-25 16:39:32 +0530

    Update NavBar.js

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -17,7 +17,7 @@ const NavBar = (props) => {
             <nav className="navbar navbar-dark bg-dark navbar-expand-lg ">
                 <div className="container-fluid">
                     <Link className="navbar-brand" to="/">iNotebook</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent" data-toggle="collapse" data-target=".navbar-collapse">
